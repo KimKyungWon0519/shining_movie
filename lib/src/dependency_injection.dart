d415aa464268df24ce340c2a4440cffe91fa96d3
KimKyungWon0519 <gwon080870@gmail.com>
2022-11-23 23:32:04 +0900

    FEAT: Add kofic repository di

--- lib/src/dependency_injection.dart
+++ lib/src/dependency_injection.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:shining_movie/src/feature/data/repositories/kofic_repository_impl.dart';
+import 'package:shining_movie/src/feature/domain/repositories/kofic_repository.dart';
 
 import 'core/values/constants.dart';
 import 'feature/data/data_source/remote/kofic_api.dart';
@@ -11,4 +13,5 @@ void init() {
   ));
 
   KoficApi koficApi = KoficApi(dio);
+  KoficRepository koficRepository = KoficRepositoryImpl(koficApi: koficApi);
 }
